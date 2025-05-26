4f14938c5fa11a46f8aec6c28d4984470033f395
KendoTMJJ <julianmontoua@gmail.com>
2025-05-26 00:09:29 -0500

    Eliminacion de endpoint redundante

--- src/submissions/submissions.controller.ts
+++ src/submissions/submissions.controller.ts
@@ -32,19 +32,4 @@ export class SubmissionsController {
         return this.submissionsService.findOne(id);
     }
 
-    @Get("all/:id")
-    findAllById(
-        @Query('problemId') problemId?: string,
-        @Query('userId') userId?: string,
-    ) {
-        if (problemId) {
-            return this.submissionsService.findByProblem(problemId);
-        }
-
-        if (userId) {
-            return this.submissionsService.findByUser(userId);
-        }
-
-        return this.submissionsService.findAll();
-    }
 }
